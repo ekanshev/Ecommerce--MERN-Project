f1d69e4d06ded74537137d66da965ed43c32e767
ekanshev <ekanshev.06@gmail.com>
2021-11-23 17:40:36 +0530

    Adding Admin/Products/Users -- Routes and Handle ErrorHander

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,20 @@ const express = require("express");
 
 const app = express();
 
+const cookieParser = require("cookie-parser");
+
 const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
+app.use(cookieParser());
 
 //Route Import
 
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoutes");
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware for Errors
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,18 +5,32 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllproduct);
 
-router.route("/product/new").post(createProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
 router
-  .route("/product/:id")
-  .put(updateProduct)
-  .delete(deleteProduct)
-  .get(getProductDetails);
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
